bcc01143b9b684a0e7615da1d5a0b2b62f469849
Aswini Metla <84316487+aswinimetla2703@users.noreply.github.com>
2022-10-27 09:53:18 +0530

    import java.util.Scanner; class tri{     public static void main(String args[]){         char c;         Scanner sc=new Scanner(System.in);         c=sc.nextLine().charAt(0);         System.out.println((int)c);         sc.close();     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class tri{
+    public static void main(String args[]){
+        char c;
+        Scanner sc=new Scanner(System.in);
+        c=sc.nextLine().charAt(0);
+        System.out.println((int)c);
+        sc.close();
+    }
+}

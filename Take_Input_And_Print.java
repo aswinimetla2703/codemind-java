a262801eff20e4344ac7a609f78c0ac3af343bc7
Aswini Metla <84316487+aswinimetla2703@users.noreply.github.com>
2022-10-27 09:59:57 +0530

    import java.util.Scanner; class inp{     public static void main(String args[]){         String v;         Scanner sc=new Scanner(System.in);         v=sc.nextLine();         System.out.println(v);         sc.close();     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class inp{
+    public static void main(String args[]){
+        String v;
+        Scanner sc=new Scanner(System.in);
+        v=sc.nextLine();
+        System.out.println(v);
+        sc.close();
+    }
+}

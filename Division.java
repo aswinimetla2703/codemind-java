96b797ddd749b89fa08025cffd5d9e11848b5a0e
Aswini Metla <84316487+aswinimetla2703@users.noreply.github.com>
2022-10-27 09:56:53 +0530

    import java.util.Scanner; class avg{     public static void main(String args[]){         int v,k;         Scanner sc=new Scanner(System.in);         v=sc.nextInt();         k=sc.nextInt();         System.out.println(v/k);         sc.close();     } }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class avg{
+    public static void main(String args[]){
+        int v,k;
+        Scanner sc=new Scanner(System.in);
+        v=sc.nextInt();
+        k=sc.nextInt();
+        System.out.println(v/k);
+        sc.close();
+    }
+}
